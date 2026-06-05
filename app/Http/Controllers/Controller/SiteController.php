064dc8730204f1995d31e6e14bea6b8934abb17c
Nadia Medina <nadiacarolinamedina@gmail.com>
2026-06-05 07:47:16 -0300

    fix: separar SiteController y corregir rutas

--- app/Http/Controllers/Controller/SiteController.php
+++ app/Http/Controllers/Controller/SiteController.php
@@ -1,36 +1,33 @@
-<?php 
- 
-namespace App\Http\Controllers; 
- 
-use Illuminate\Http\Request; 
-use App\Models\Producto; // <-- IMPORTANTE: Importar el modelo 
- use App\Models\Provincia;
-class SiteController extends Controller 
-{ 
-    public function inicio() 
-    { 
-        return view('inicio'); 
-    } 
- 
-    public function productos() 
-    { 
-        // El controlador le pide los datos al Modelo 
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Producto;
+use App\Models\Provincia;
+
+class SiteController extends Controller
+{
+    public function inicio()
+    {
+        return view('inicio');
+    }
+
+    public function productos()
+    {
         $productos = Producto::allLocal();  
- 
-        return view('productos', compact('productos')); 
-    } 
- 
-    public function contacto() 
-    { 
-        $email = 'contacto@miempresa.com'; 
-        return view('contacto', compact('email')); 
-    } 
+        return view('productos', compact('productos'));
+    }
 
-        public function provincias() 
-        { 
-            $provincias = Provincia::all();  
-    
-            return view('provincias', compact('provincias')); 
-        }
+    public function contacto()
+    {
+        $email = 'contacto@miempresa.com';
+        return view('contacto', compact('email'));
+    }
 
-} 
+    public function provincias()
+    {
+        $provincias = Provincia::all();
+        return view('provincias', compact('provincias'));
+    }
+}
